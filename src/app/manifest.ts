edaae29d27390f4269f8e2b74490d8f87aa2c71c
Kyle Friel <businessfriel@gmail.com>
2025-02-22 18:04:06 -0500

    manifest and favicon

---
+++ src/app/manifest.ts
@@ -0,0 +1,31 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Kyle Friel | Portfolio",
+    short_name: "Kyle Friel",
+    description: "Kyle Friel's Portfolio powered by Next.js",
+    lang: "en",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#18181b",
+    theme_color: "#18181b",
+    icons: [
+      {
+        src: "/images/icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/images/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
+      },
+    ],
+  };
+}
