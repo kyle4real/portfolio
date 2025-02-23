5165fda1f2c70c5765d94b2d76d4edbeda4ccd39
Kyle Friel <businessfriel@gmail.com>
2025-02-22 21:02:23 -0500

    make spotlight stay on pointer by changing fixed to absolute

--- src/components/spotlight.tsx
+++ src/components/spotlight.tsx
@@ -11,7 +11,9 @@ export const Spotlight: React.FC<SpotlightProps> = ({ children }) => {
   const containerRef = useRef<HTMLDivElement>(null);
 
   const onMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
-    if (!containerRef.current) return;
+    if (!containerRef.current) {
+      return;
+    }
 
     const div = containerRef.current;
     const rect = div.getBoundingClientRect();
@@ -27,7 +29,7 @@ export const Spotlight: React.FC<SpotlightProps> = ({ children }) => {
       {children}
 
       <div
-        className="pointer-events-none z-30 fixed inset-0 transition duration-300 lg:block hidden"
+        className="pointer-events-none z-30 absolute inset-0 transition duration-300 lg:block hidden"
         style={{
           opacity: 1,
           background: `radial-gradient(600px at ${position.x}px ${position.y}px, rgb(142, 197, 255, 0.06), transparent 80%)`,
